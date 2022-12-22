750eb87270409a0f465d10ff0759a8790e1db2aa
jessica <jessica7rm@gmail.com>
2022-12-22 18:51:44 -0300

    primer_commit

---
+++ src/app/componentes/encabezado/encabezado.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-component',
+  templateUrl: './encabezado.component.html',
+  styleUrls: ['./encabezado.component.css']
+})
+export class EncabezadoComponent {
+
+}
